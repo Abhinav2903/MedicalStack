f7411bd70cfa9ab25b0c8906496ae130ee931008
Yogesh Choudhary <youzendachoudhary22@gmail.com>
2020-10-28 05:11:44 +0530

    use of home in the material design widget instead of route

--- lib/main.dart
+++ lib/main.dart
@@ -6,23 +6,13 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  var MED_STACK;
 
-  // String MED_STACK;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: new MaterialApp(initialRoute: '/', routes: <String, WidgetBuilder>{
-        MED_STACK: (BuildContext context) => new Second(),
-      }),
+    return new MaterialApp(
+      home: Second(),
     );
   }
 
-  // @override
-  // First createState() => First();
 }
-
-// class First extends State<MyApp> {
-
-// }
